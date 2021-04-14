0477e4ca853c630bd9da0adf7a57e6cd0aa2dd12
Lara Komosa <lara.komosa@artisantechgroup.com>
2021-04-14 13:07:29 -0500

    search function working and rendering

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
 import {Button, ButtonToolbar, Row, Col} from 'react-bootstrap';
-
+import {EditDepModal} from './EditDepModal';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 export class Home extends Component{
     state = {
-        name: ""
+        name: "",
+        array: [], 
+        editModalShow:false
       };
-
 
     handleInputChange = (name) => (event) => {
         this.setState({
@@ -18,28 +26,48 @@ export class Home extends Component{
       };
 
     SearchDep(name){
-        fetch(process.env.REACT_APP_API + `TodoItems/search?name`+ this.state.name)
-        console.log('search name', this.state.name)
-            .then(response=>response.json());
-  
-        }
-     
+        console.log ('searchDep', this.state.name)
+        fetch(process.env.REACT_APP_API + `TodoItems/search?name=${this.state.name}`)
+        .then(response => response.json())
+      .then(responseData => {
+        this.setState({
+          array: responseData.data,
+          loading: false
+        });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+  }
 
-            
-              render() {
+        
+        render() {
+            const {array} = this.state;
+            console.log(array)
+
                 return (
-                  <form>
+                  <><form>
                     <input
                       placeholder="Search for..."
                       value={this.state.name}
                       onChange={this.handleInputChange('name')}
                     />
-                    <Button onClick ={this.SearchDep('name')}>Search</Button>
-                    <p>{this.state.query}</p>
+                    <Button onClick={() => this.SearchDep()}>Search</Button>
+                    <p>{this.state.deps}</p>
                   </form>
-                );
-              }
-            }
+                  <TableBody>
+        {array.map(object=>
+            <TableRow key={object.id}>
+              <TableCell component="th" scope="row">
+                {object.name}
+              </TableCell>
+              <TableCell align="right">{object.isComplete.toString()}</TableCell>
+        </TableRow>
+        )}
+     </TableBody>
+                 </>
+            
+                )}}
             
             export default Home;   
 
--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,8 +1,15 @@
 import React, {Component} from 'react';
-import {Table} from 'react-bootstrap';
 import {Button, ButtonToolbar, Row, Col} from 'react-bootstrap';
 import {AddDepModal} from './AddTaskModal';
 import {EditDepModal} from './EditDepModal';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 export class TodoItems extends Component{
 
@@ -42,58 +49,54 @@ fetch(process.env.REACT_APP_API+ `TodoItems/`+ depid,{
         const {deps, depid, depname, depisComplete} = this.state;
         let addModalClose=()=>this.setState({addModalShow:false})
         let editModalClose=()=>this.setState({editModalShow: false})
-        return(
-            <div>
-        Weekly To Do List
-        <Row className = "To Do Table">
-            <Col lg={{span: 8}} md={{span: 8}} >
-                
-                <Table className="mt-4" strinped bordered hover size="sm">
-                <thead>
-                    <tr>
-                    <td>Todo Task</td>
-                    <td>Completed?</td>
-                    <td> Adjust</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {deps.map(dep=>
-                        <tr key = {dep.id}>
-                            <td>{dep.name}</td>
-                            <td>{dep.isComplete.toString()}</td>
-                            <td>  
-                            <ButtonToolbar>
-                                <Button className="mr-2" variant="info"
-                                onClick={()=>this.setState({editModalShow:true.valueOf,
-                                depid:dep.id, depname: dep.name, depisComplete: dep.isComplete})}>
-                                    Edit
-                                </Button>
-                                <Button className="mr-2" variant="info"
-                                onClick={()=>this.deleteDep(dep.id)}>
-                                    Delete
-                                </Button>
-                                <EditDepModal show ={this.state.editModalShow}
-                                onHide={editModalClose}
-                                depid= {depid}
-                                depname= {depname}
-                                depisComplete= {depisComplete}/>
-                            </ButtonToolbar>
-                            </td>
-                        </tr>
-                        
-                        )}
-                </tbody>
-                </Table>
-                <ButtonToolbar>
-                    <Button variant='primary'
-                    onClick={()=>this.setState({addModalShow: true})}>
-                    Add Task</Button>
-                    <AddDepModal show={this.state.addModalShow}
-                    onHide={addModalClose}/>
-                </ButtonToolbar>
-            </Col>
-        </Row>
-    </div>
-        )
-    }
-}
+        return(<>
+    <TableContainer component={Paper}>
+      <Table aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Todo Item (100g serving)</TableCell>
+            <TableCell align="right">Completed</TableCell>
+            <TableCell align="right">Adjust Item&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {deps.map(dep=>
+            <TableRow key={dep.id}>
+              <TableCell component="th" scope="row">
+                {dep.name}
+              </TableCell>
+              <TableCell align="right">{dep.isComplete.toString()}</TableCell>
+              <TableCell>
+              <ButtonToolbar>
+              <Button className="mr-2" variant="info"
+              onClick={()=>this.setState({editModalShow:true.valueOf,
+              depid:dep.id, depname: dep.name, depisComplete: dep.isComplete})}>
+                  Edit
+              </Button>
+              <Button className="mr-2" variant="info"
+              onClick={()=>this.deleteDep(dep.id)}>
+                  Delete
+              </Button>
+              <EditDepModal show ={this.state.editModalShow}
+              onHide={editModalClose}
+              depid= {depid}
+              depname= {depname}
+              depisComplete= {depisComplete}/>
+          </ButtonToolbar>
+          </TableCell>
+        </TableRow>
+        )}
+     </TableBody>
+     </Table>
+     </TableContainer>
+      <ButtonToolbar>
+      <Button variant='primary'
+      onClick={()=>this.setState({addModalShow: true})}>
+      Add Task</Button>
+      <AddDepModal show={this.state.addModalShow}
+      onHide={addModalClose}/>
+  </ButtonToolbar>
+  </>
+  );}}
+
+  export default TodoItems;  
